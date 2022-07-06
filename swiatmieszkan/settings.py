d3cfc109dedf4f868ef42f96d9f18039c1b97b66
Karol Sliwka <44789604+KarolSliwka@users.noreply.github.com>
2022-07-06 20:15:40 +0200

    Django sercret key changed

--- swiatmieszkan/settings.py
+++ swiatmieszkan/settings.py
@@ -23,7 +23,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY', '')
+SECRET_KEY = os.environ.get('DJANGO_SECRET', ' ')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMET' in os.environ
