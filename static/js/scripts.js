675962c8f0b78d2bb96f5f665ff173d141436559
Karol Sliwka <44789604+KarolSliwka@users.noreply.github.com>
2022-07-10 13:07:55 +0200

    Visibility function added

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,15 +1,12 @@
 $(document).ready(function () {
-  // NAVIGATION
-  /**
-   * This function fill change hamburegr button from lines to cross sign on small devices
-   */
-  $("#hamburger-button").click(function () {
+  $("#ham-btn-med").click(function () {
     $(this).toggleClass("open");
-  });
-  /**
-   * This function fill change hamburegr button from lines to cross sign on medium devices
-   */
-  $("#hamburger-button-medium").click(function () {
-    $(this).toggleClass("open");
+
+    // chceck if sub-menu has class open then change display to block otherwise change it back to none
+    if ($("#ham-btn-med").hasClass("open")) {
+      $(".sub-menu-1").css("visibility", "visible");
+    } else {
+      $(".sub-menu-1").css("visibility", "hidden");
+    }
   });
 });
