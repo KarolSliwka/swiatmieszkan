c62144adbf27435cfcbf8ed6bfb5c5585fc6d981
Karol Sliwka <44789604+KarolSliwka@users.noreply.github.com>
2022-07-31 16:54:17 +0200

    Home views added

--- home/views.py
+++ home/views.py
@@ -5,14 +5,29 @@ from django.contrib import messages
 
 
 def home(request):
-
-    context = {
-    }
-
-    return render(request, 'home/home.html', context)
+    """ A view to display homepage """
+    return render(request, 'home/home.html')
 
 
-@staff_member_required
-def remove_lessons_cards_management(request, card_id):
+def landlords(request):
+    """ A view to display information for landlords """
 
-    return redirect(reverse('home'))
+    return render(request, 'home/landlords.html')
+
+
+def tenants(request):
+    """ A view to display information for tenants """
+
+    return render(request, 'home/tenants.html')
+
+
+def about(request):
+    """ A view to display information about platfrom """
+
+    return render(request, 'home/about.html')
+
+
+def information(request):
+    """ A view to view main information sections """
+
+    return render(request, 'home/information.html')
