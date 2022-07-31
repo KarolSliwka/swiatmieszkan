484b168d2b61062834127f756a65918c20345a6e
Karol Sliwka <44789604+KarolSliwka@users.noreply.github.com>
2022-07-31 16:54:24 +0200

    Urls created

--- home/urls.py
+++ home/urls.py
@@ -6,4 +6,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('informacje/wynajmujacy', views.landlords, name='landlords'),
+    path('informacje/najemca', views.tenants, name='tenants'),
+    path('informacje/o-platformie', views.about, name='about'),
+    path('informacje', views.information, name='information'),
 ]
